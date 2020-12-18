2819ba7b589a468584e9f259449853d86aefefe1
Rago <63511519+ragokan@users.noreply.github.com>
2020-12-18 20:23:35 +0300

    improvements for frontend

--- src/translateFiles.js
+++ src/translateFiles.js
@@ -1,6 +1,16 @@
 const fs = require("fs");
 const path = require("path");
+let translateFiles;
 
-const translateFiles = JSON.parse(fs.readFileSync(path.resolve("lokaly.json")));
+if (typeof window === "undefined") {
+  translateFiles = JSON.parse(fs.readFileSync(path.resolve("lokaly.json")));
+} else {
+  const blob = new Blob([json], { type: "application/json" });
+  const reader = new FileReader();
+  reader.addEventListener("load", (e) => {
+    path.resolve("lokaly.json");
+  });
+  translateFiles = JSON.parse(reader.readAsText(blob));
+}
 
 module.exports = translateFiles;
